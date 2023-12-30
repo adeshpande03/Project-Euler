b9807c873d1e39dba90f663e9bc37372869b1656
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-12-30 22:29:00 +0530

    206, 69 lol 69 sanni send dog

---
+++ 0206.py
@@ -0,0 +1,23 @@
+def main():
+    i = 10**9
+    while True:
+        s = str(i**2)
+        if len(s) > 18:
+            if (
+                s[0] == "1"
+                and s[2] == "2"
+                and s[4] == "3"
+                and s[6] == "4"
+                and s[8] == "5"
+                and s[10] == "6"
+                and s[12] == "7"
+                and s[14] == "8"
+                and s[16] == "9"
+                and s[18] == "0"
+            ):
+                print(i)
+                break
+        i += 1
+
+
+main()
