09c55e88986fa3701973fd144cb1afc15eb23bb7
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2023-12-31 22:11:37 +0530

    187 and i feel really smart

--- 0187.py
+++ 0187.py
@@ -5,13 +5,13 @@ from itertools import combinations_with_replacement
 
 def main():
     d = 0
-    p = listPrimes2(10**8 // 2)
+    p = listPrimes2(10**7 * 5)
     print()
     for i in range(len(p)):
+        if p[i] ** 2 > 10**8:
+            break
         for j in range(i, len(p)):
-            if p[i] ** 2 > 10**8:
-                break
-            elif p[i] * p[j] < 10**8:
+            if p[i] * p[j] < 10**8:
                 d += 1
             else:
                 break
